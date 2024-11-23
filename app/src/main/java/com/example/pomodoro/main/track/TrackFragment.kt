8f8620115e7bf241b646719d6b6a7854d63d46d7
Haneol <haneolj@gmail.com>
2024-11-23 16:36:13 +0900

    [feature/timer] chore: 타이머 구현을 위한 테스트용 fragment 구성

--- app/src/main/java/com/example/pomodoro/main/track/TrackFragment.kt
+++ app/src/main/java/com/example/pomodoro/main/track/TrackFragment.kt
@@ -10,6 +10,11 @@ import com.example.pomodoro.databinding.FragmentTrackBinding
 class TrackFragment : Fragment() {
     private var _binding: FragmentTrackBinding? = null
     private val binding get() = _binding!!
+    private var isStart = false
+        set(value) {
+            field = value
+            updateButtonUI()
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,9 +33,34 @@ class TrackFragment : Fragment() {
 
     private fun setupViews() {
         // 뷰 설정 및 이벤트 처리 코드
-        binding.textView2.setOnClickListener {
-            // 클릭 이벤트 처리
+        binding.timerButton2.apply {
+            text = "시작"  // 초기 상태
+            setOnClickListener {
+                isStart = !isStart  // 상태 토글만 하면 UI는 자동으로 업데이트
+                handleTimerState()
+            }
         }
+
+    }
+
+    private fun updateButtonUI() {
+        binding.timerButton2.text = if (isStart) "중지" else "시작"
+    }
+
+    private fun handleTimerState() {
+        if (isStart) {
+            // startTimer()
+        } else {
+            // stopTimer()
+        }
+    }
+
+    private fun startTimer() {
+
+    }
+
+    private fun stopTimer() {
+
     }
 
     override fun onDestroyView() {
